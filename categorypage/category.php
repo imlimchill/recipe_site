7c71589ea16c10fab4a59b6b0417f524e7436229
hyunwoooh1230 <bomul1230@gmail.com>
2020-11-02 10:34:43 +0900

    2020-11-02 메인페이지와 레시피 페이지 연결 카테고리 페이지 db 연결 레시피 작성 db 연결 레시피 페이지 좋아요 기능 추가

--- categorypage/category.php
+++ categorypage/category.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include "./db/db.php";
+include $_SERVER['DOCUMENT_ROOT']."/recipe_site/db/db.php";
 ?>
     
 
@@ -117,10 +117,11 @@ include "./db/db.php";
                         <span class="caret"></span> 放送局
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">3大天王</a><br>
-                        <a class="dropdown-item" href="#">골목식당</a><br>
-                        <a class="dropdown-item" href="#">맛남의광장 </a><br>
-                        <a class="dropdown-item" href="#">마리텔</a>
+                        <a class="dropdown-item" href="#">3代天王</a><br>
+                        <a class="dropdown-item" href="#">ゴルモクシクダン</a><br>
+                        <a class="dropdown-item" href="#">マッナムウグァンザン　 </a><br>
+                        <a class="dropdown-item" href="#">マリテル</a>
+                        <a class="dropdown-item" href="#">ベクパド</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown col-md-3">
@@ -134,7 +135,7 @@ include "./db/db.php";
                         <a class="dropdown-item" href="#">中食</a>
                     </div>
                 </li>
-                <li class="nav-item col-md-3"> <a class="nav-link" href="#">後記</a> </li>
+                <li class="nav-item col-md-3"> <a class="nav-link" href="../review/review.php">後記</a> </li>
                 <!-- <li class="nav-item col-md-20"> <a class="nav-link" href="#">マイページ</a> </li>  마이페이지-->
 
             </ul>
@@ -146,173 +147,175 @@ include "./db/db.php";
         <div class="row text-center">
             <p class="check_box_title"><strong>カテゴリー</strong></p>
             <div class="btn-group-toggle .btn-style " data-toggle="buttons">
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                <label class="btn btn-primary col-md-2 checked="checked" disabled="disabled">
+                    <input type="checkbox" > ユーチューブ
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> スンウアパ
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> ベクス 料理秘策
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> ハルハンキ 
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> モクオボルレ 
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-            </div>
-            <div class="btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
-                </label>
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox">Yonam
                 </label>
             </div>
             <div class="btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                <label class="btn btn-primary col-md-2" checked="checked" disabled="disabled">
+                    <input type="checkbox"> 放送
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> 3代天王
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> ゴルモクシ食堂
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> マッナムウ広場
                 </label>
                 <label class="btn btn-primary col-md-2">
-                    <input type="checkbox"> Checkbox
+                    <input type="checkbox"> マリテル
+                </label>
+                <label class="btn btn-primary col-md-2">
+                    <input type="checkbox"> ベクパド
+                </label>
+            </div>
+            <div class="btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-primary col-md-2" checked="checked" disabled="disabled">
+                    <input type="checkbox"> テーマ
+                </label>
+                <label class="btn btn-primary col-md-2">
+                    <input type="checkbox"> 韓食
+                </label>
+                <label class="btn btn-primary col-md-2">
+                    <input type="checkbox"> 和食
+                </label>
+                <label class="btn btn-primary col-md-2">
+                    <input type="checkbox"> 洋食
+                </label>
+                <label class="btn btn-primary col-md-2">
+                    <input type="checkbox"> 中食
+                </label>
+                <label class="col-md-2 search"> 
+                    <input type="submit" value="檢索" class="cg_btn"> 
                 </label>
             </div>
         </div>
     </div>
     <br>
+    <?php
+     if(isset($_GET['page'])){
+        $page=$_GET['page'];
+    }else{
+        //$_GET['page']안에 값이 없을 경우 $page 값에 1을 입력
+        $page=1;
+    }
+    //comment 테이블에서 comment_id를 기준으로 오름차순해서 0부터 5개를 입력
+    //limt 0(시작할 위치) 5(출력할 갯수)출력
+    $sql2 = mq("select * from po_recipe order by recipe_seq");
+    //mysqli_num_rows(함수) : sql의 레코드의 행을 구함, 게시판의 총 레코드 수
+    $row_num = mysqli_num_rows($sql2);
+    //블록당 보여줄 페이지의 개수
+    $block_ct = 5;
+    //현재 페이지의 블록 구하기
+    //ceil flooer round함수
+    //ceil은 입력값의 소수부분이 존재할 때 값을 올려서 리턴하는 함수
+    //floor은 입력값을 소수부분이 존재할 때 값을 내려서 리턴하는 함수
+    //round는 입력값에 소수부분이 0.5보다 크면 값을 올리고 작으면 값을 버린 후
+    //리턴하는 반올림 함수
+    //$block_num은 현재 페이지의 블록 구하기
+    $block_num= ceil($page/$block_ct);
+    //블록의 시작번호
+    $block_start =(($block_num-1)*$block_ct)+1;
+    $block_end =$block_start+$block_ct-1;
+    //페이징한 페이지의 숫자 구하기.
+    //전체 글의 개수는 $row_num 한 페이지당 나오는 글의 개수는 5개이기 때문에b
+    //따라서 전체 페이지는 전체 글의 개수나누기 한 페이지당 나오는 글의 수로 나눈다
+    $total_page = ceil($row_num/5);
+    //만약 블록의 마지막 번호가 페이지 수보다 많다면
+    if($block_end>$total_page){
+        $black_end=$total_page;
+    }
+    //블록의 총 개수 구하기
+    $total_block = ceil($total_page/$block_ct);
+    //시작번호 ($page-1)에서 5를 곱한다
+    $start_num = ($page-1) * 5;
+    ?>
     <div class="container recipe_recommendation_box">
         <div class="row content_box1 img-fluid">
             <div class="content_box1_title">
-                <div class="content_box1_title_content row">オ·ヒョヌ</div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <a href="#"><img class="card-img-top img-responsiveimg-rounded" src="../img/exam1.jpg " alt=""></a>
+                <div class="content_box1_title_content row">おすすめレシピ</div>
+                <?php
+                $sql3 = mq("select * from po_recipe order by recipe_likes desc limit $start_num,12");
+                    while($recipe_info = $sql3->fetch_array()){
+                        ?>
+                    
+                <div class="col-md-3 col-sm-3">
+                    <a href="table.php?recipe_seq=<?php echo $recipe_info["recipe_seq"];?>"><img class="card-img-top img-responsive img-rounded" src="http://localhost/recipe_site/img/<?php echo $recipe_info["img"];?>" style="width: 212px; height: 160px;" text-center></a>
                     <div class="card-body">
                         <h4 class="card-title">
-                            <a href="#">된장찌개</a>
+                            <a href="#"><?php echo $recipe_info["recipe_name"];?>&nbsp;</a>
                         </h4>
-                        <h5>食べなさい</h5>
-                        <p class="card-text">이맛에 반해요</p>
+                        <h5>좋아요 수:<td><?php echo $recipe_info["recipe_likes"];?>&nbsp;</td></h5>
+                        <p class="card-text">작성자: <?php echo $recipe_info["recipe_name"];?>&nbsp;</p>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <a href="#"><img class="card-img-top img-responsive img-rounded" src="../img/exam1.jpg" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">된장찌개</a>
-                        </h4>
-                        <h5>드셔보세요</h5>
-                        <p class="card-text">이맛에 반해요</p>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top img-responsive img-rounded" src="../img/exam1.jpg"
-                                alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">된장찌개</a>
-                            </h4>
-                            <h5>드셔보세요</h5>
-                            <p class="card-text">이맛에 반해요</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top img-responsive  img-rounded" src="../img/exam1.jpg"
-                                alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">된장찌개</a>
-                            </h4>
-                            <h5>드셔보세요</h5>
-                            <p class="card-text">이맛에 반해요</p>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                    }
+                 ?> 
             </div>
         </div>
     </div>
-    <br>
-    <div class="container recipe_recommendation_box">
-        <div class="row content_box1 img-fluid">
-            <div class="content_box1_title">
-                <div class="content_box1_title_content row">オ·ヒョヌ</div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <a href="#"><img class="card-img-top img-responsiveimg-rounded" src="../img/exam1.jpg " alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">된장찌개</a>
-                        </h4>
-                        <h5>食べなさい</h5>
-                        <p class="card-text">이맛에 반해요</p>
+    <div class="text-center">
+                    <div class='page-item'>
+                        <?php echo "[".$page."]"; 
+                        for($i=$block_start; $i<=$block_end; $i++){
+                            //페이지와 $i가 같지 않을 시에 숫자를 출력한다
+                            if($page != $i){
+                                //페이지 숫자를 클릭할 시 ($_GET('page'))$next 변수를 삽입
+                                echo "<span class='page-item'><a href='
+                                ?page=$i'>[$i]</a></span>";
+                            }
+                        }  
+                        ?> 
+                    </div>    
+                    <ul class="pagination"> 
+                        
+                        <?php
+                            //현재 페이지가 1을 초과했을 때 출력한다.
+                            if($page >1){
+                                //처음 버튼을 누를 시에 ($_GET('page')값에 1을 삽입)
+                                echo "<li class='page-item'><a href='?page=1'>처음</a></li>";
+                            }
+                            //현재 페이지가 1을 초과했을 때 출력한다.
+                            if($page >1){
+                                //$pre 변수에 $page-1을 해준다.
+                                $pre = $page-1;
+                                //이전 버튼을 클릭할 시에 ($_GET('page')값에 $pre변수를 삽입
+                                echo "<li class='page-item'><a href='?page=$pre'>이전</a></li>";
+                            }
+                                      
+                            //만약에 현재 블록이 블록의 총 갯수 미만일 경우
+                            if($page < $total_page){
+                                //$next 변수에 $page변수에 1을 더한 값을 삽입
+                                $next = $page+1;
+                                // 다음 버튼을 클릭할 시 ($_GET('page')값에 $next 변수를 삽입
+                                echo "<li class='page-item'><a href='?page=$next'>다음</a></li>";
+                            }
+                            //만약에 page가 총 페이지 수의 미만일 경우
+                            if($page<$total_page){
+                                //마지막 버튼을 클릭할 시($_GET('page')값에 총 페이지 수를 삽입
+                                echo "<li class='page-item'><a href='?page=$total_page'>마지막</a></li>";
+                            }
+                        ?>
+                    </ul>
                     </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <a href="#"><img class="card-img-top img-responsive img-rounded" src="../img/exam1.jpg" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">된장찌개</a>
-                        </h4>
-                        <h5>드셔보세요</h5>
-                        <p class="card-text">이맛에 반해요</p>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top img-responsive img-rounded" src="../img/exam1.jpg"
-                                alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">된장찌개</a>
-                            </h4>
-                            <h5>드셔보세요</h5>
-                            <p class="card-text">이맛에 반해요</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-3 col-3">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top img-responsive  img-rounded" src="../img/exam1.jpg"
-                                alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">된장찌개</a>
-                            </h4>
-                            <h5>드셔보세요</h5>
-                            <p class="card-text">이맛에 반해요</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <br>
     </div>
     <div class="container-fluid footer_wrap">
         <footer class="footer_margin">
